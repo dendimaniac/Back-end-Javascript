601e9f8dc02b4745b724a8a5e551a1c4c756c086
Dendi Maniac <daq30899@gmail.com>
2019-11-20 15:02:45 +0200

    Changed into sql query to database, with getting all + getting 1 cat

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -7,21 +7,9 @@ const catController = require('../controllers/catController');
 
 router.get('/', catController.cat_list_get);
 
-router.get('/:id', (req, res) => {
-    res.send(`You requested a cat whose id is ${req.params.id}`);
-});
+router.get('/:id', catController.cat_get);
 
-// router.post('/', (req, res) => {
-//     res.send('With this endpoint you can add cats.');
-// });
-
-router.post('/', upload.single('cat'), function (req, res, next) {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
-    console.log(req.file);
-    res.send('Sent file');
-    next();
-});
+router.post('/', upload.single('cat'), catController.cat_create_post);
 
 router.put('/', (req, res) => {
     res.send('With this endpoint you can edit cats.');
