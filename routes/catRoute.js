53f048119f10c584398873451ceabccff2ef9c25
Dendi Maniac <daq30899@gmail.com>
2019-11-20 12:37:56 +0200

    Added file upload with multer

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,6 +1,8 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const catController = require('../controllers/catController');
 
 router.get('/', catController.cat_list_get);
@@ -9,8 +11,16 @@ router.get('/:id', (req, res) => {
     res.send(`You requested a cat whose id is ${req.params.id}`);
 });
 
-router.post('/', (req, res) => {
-    res.send('With this endpoint you can add cats.');
+// router.post('/', (req, res) => {
+//     res.send('With this endpoint you can add cats.');
+// });
+
+router.post('/', upload.single('cat'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    console.log(req.file);
+    res.send('Sent file');
+    next();
 });
 
 router.put('/', (req, res) => {
